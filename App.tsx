b802aa654f3f66025b868ab758447cb1d032ebcc
DoTrungHieu2004 <hieu10productions@gmail.com>
2025-04-09 12:46:34 +0700

    Adding Poppins fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins: require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <StackNavigator />
